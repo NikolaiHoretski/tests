a1f98f958010d64595b3d44d7d38dcee8e7231ad
nikolai.horetski <nikolai.horetski@gmail.com>
2026-05-05 11:45:13 +0300

    feat: add start link in log

---
+++ src/main/java/com/nikolaihoretski/tests/startLink/ApplicationStartLinkInLog.java
@@ -0,0 +1,25 @@
+package com.nikolaihoretski.tests.startLink;
+
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class ApplicationStartLinkInLog {
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void logApplicationUrl(@NonNull ApplicationReadyEvent event) {
+        final Environment environment = event.getApplicationContext().getEnvironment();
+        final String port = environment.getProperty("local.server.port");
+        final String contextPath = environment.getProperty("server.servlet.context-path", "");
+
+        final String baseUrl = String.format("http://localhost:%s%s", port, contextPath);
+
+        log.info("\nSwagger UI: http://localhost:{}{}/swagger-ui/index.html", port, contextPath);
+    }
+
+}
